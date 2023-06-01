339bda0aff8437fa4bc5650c5fcfc64e002f448a
ericarodri <erica.cunha@sptech.school>
2023-06-01 15:31:34 -0300

    dashboard conectado ao banco, porém inserindo apenas manualmente

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -31,8 +31,22 @@ function cadastrar(nome, email, senha) {
     return database.executar(instrucao);
 }
 
+function votar(idusuario, idpersonagem) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", idusuario, idpersonagem);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO voto (fkUser, fkPersonagem) VALUES (${idusuario}, ${idpersonagem});
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+
 module.exports = {
     entrar,
     cadastrar,
     listar,
+    votar
 };
--- site/src/routes/usuarios.js
+++ site/src/routes/usuarios.js
@@ -16,6 +16,10 @@ router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
 })
 
+router.post("/votar", function (req, res) {
+    usuarioController.votar(req, res);
+})
+
 router.post("/autenticar", function (req, res) {
     usuarioController.entrar(req, res);
 });
